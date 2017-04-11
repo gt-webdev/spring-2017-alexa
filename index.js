0c445387316dfe272e948639c75638a8b5011276
Jackson Morgan <m@Jacksons-MacBook-Air.local>
2017-04-11 16:47:24 -0400

    Updated to work with Alexa

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var bodyParser = require('bodyParser');
 
 var app = express();
 
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
-  res.send('working');
+  var response = {
+    "version": "1.0",
+    "response": {
+      "outputSpeech": {
+        "type": "PlainText",
+        "text": "I like " + req.body.request.intent.slots.subject.value
+      }
+    }
+  }
+
+  res.send(response);
 });
 
 var port = process.env.PORT || 3000;
